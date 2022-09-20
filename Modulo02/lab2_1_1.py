65e7960bfebc91af7d842d73c639a8e2f8c2bfc5
pablo0121 <pablopalma012@gmail.com>
2022-09-20 08:42:04 -0500

    Laboratorio 2_1_1_6

---
+++ Modulo02/lab2_1_1.py
@@ -0,0 +1,6 @@
+'''
+Descripción: Esto es un programa para realizar los comandos básicos de python
+'''
+print("¡Hola mundo!")
+print("Juan Pablo Palma")
+print("Bienvenido a la certificación de Python 3")
