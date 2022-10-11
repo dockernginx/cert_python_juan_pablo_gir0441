47696179ecf173526b07d4b1450476d8015822dc
pablo0121 <pablopalma012@gmail.com>
2022-10-11 08:39:34 -0500

    Modulos

--- Modulo03/lab3_1_1_12.py
+++ Modulo03/lab3_1_1_12.py
@@ -5,7 +5,9 @@ Descripción: Esto es un programa para realizar los comandos básicos de python 
 introdujistes es bisiesto o común. 
 '''
 year = int(input("Introduce un año: "))
-if year % 4 !=0: 
+if(year <= 1582): 
+    print('No esta dentro del período del calendario Gregoriano')
+elif year % 4 !=0: 
     print('Año Común') 
 elif year % 100 !=0: 
     print('Año Bisiesto')
