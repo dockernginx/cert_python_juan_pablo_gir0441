90e516fdb37a8ac0794d504bbf89fb215007a99f
pablo0121 <pablopalma012@gmail.com>
2022-10-13 08:47:53 -0500

    Modulos y Examen

--- Modulo03/lab3_1_1_4.py
+++ Modulo03/lab3_1_1_4.py
@@ -4,4 +4,4 @@ Fecha:  20 / sep /2022
 Descripción: Esto es un programa para realizar los comandos básicos de python
 '''
 n = int(input('Ingresa un valor entero: '))
-print(n > 100)
+print(n >= 100)
