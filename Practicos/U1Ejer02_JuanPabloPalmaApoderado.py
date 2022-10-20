fe917f473954250ec6c46ee9ab364ed9ce11d822
pablo0121 <pablopalma012@gmail.com>
2022-10-20 14:57:50 -0500

    Modulos y Practicos

--- Practicos/U1Ejer02_JuanPabloPalmaApoderado.py
+++ Practicos/U1Ejer02_JuanPabloPalmaApoderado.py
@@ -1,11 +1,12 @@
 '''
 Nombre: Juan Pablo Palma Apoderado
 Fecha: 13 / oct / 2022
+Descripción: EL ejercicio 
 '''
-nom = ['Francisco', 'Pedro','Juan','Fernando']
-print("El tamaño de la lista es: ", len(nom))
-print('El contenido de la lista es: ', nom)
-nom.insert(1,'Joaquin')
-nom.append('Luis Miguel')
-for nombres in nom: 
+nom = ['Francisco', 'Pedro','Juan','Fernando'] #nombramos un  arreglo nom con los nombres de los amigos
+print("El tamaño de la lista es: ", len(nom))#Imprimimos el tamaño del arreglo con la función len
+print('El contenido de la lista es: ', nom)#Imprimimos todos los elementos del arreglo
+nom.insert(1,'Joaquin') #Insertamos en la posición 1 a Joaquin y recorremos a Pedro una posición
+nom.append('Luis Miguel')#Por ultimo agregamos comom ultimo elemento a LUIS MIGUEL
+for nombres in nom:  #Imprimimos con un FOR losm elemntos de la lista un por uno
     print(nombres)
--- Practicos/U1Ejer03_JuanPabloPalmaApoderado.py
+++ Practicos/U1Ejer03_JuanPabloPalmaApoderado.py
@@ -2,19 +2,19 @@
 Nombre: Juan Pablo Palma Apoderado
 Fecha: 13 / oct / 2022
 '''
-numeros_control = [1219100524,1219100537,1219100543,121910133,1219100484]
-nombre = []
-aml= ''
-for numcontrol in numeros_control: 
-    print("Ingresa el nombre con el No.Control ",numcontrol)
-    aml = input()
-    nombre.append(aml)
-print(nombre)
-numerotemp = 0
-while True: 
-    numerotemp = int(input("Ingresa el numero de control: "))
-    if numeros_control == numeros_control: 
-        print('El numero de control si fue encontrado')
+numeros_control = [1219100524,1219100537,1219100543,121910133,1219100484] #Agregamos un arrreglo con los numeros de control
+nombre = [] #Otro arreglo con nombres vacio
+aml= '' #Decalramos una varible de texto vacia 
+for numcontrol in numeros_control: #Recorre el for de los numeros de control
+    print("Ingresa el nombre con el No.Control ",numcontrol) #Toma cada numero de control
+    aml = input() #agregamos su nombre
+    nombre.append(aml)#Se va agregando el nombre en la posición de cada numero de control
+print(nombre)#Imprime el arreglo agregado de los nombre de los alumnos
+numerotemp = 0 #Declaramos una variable igual a 0 tomada como numero de control
+while True: #Mientras es verdadero se ejecuta el codijo
+    numerotemp = int(input("Ingresa el numero de control: "))#El usuario ingresear a la variable antes mencioanda el numero de control que quiere buscar
+    if numeros_control == numeros_control:  #Verificammos en el arreglo de numeros_control es igual a que usuario desea buscar
+        print('El numero de control si fue encontrado')#Mostrara si fue encontrado en el arreglo
     else: 
-        print('El numero de control si fue encontrado')
+        print('El numero de control si fue encontrado')#Mostrara si no fue encontrado en el arreglo
     break
